b97f4959a9dba21e8516cb47f728bd29ffe38937
antyhacker75-cmyk <antyhacker75@gmail.com>
2026-09-05 09:41:56 +0800

    Add loader for Astro Star with fallback notification
    Fetches script from a remote source and handles failure.

---
+++ loader.lua
@@ -0,0 +1,17 @@
+local src = ""
+local CoreGui = game:GetService("StarterGui")
+
+pcall(function() 
+    src = game:HttpGet("my-angge.x10.mx", false)
+end)
+if src == "" then
+  CoreGui:SetCore("SendNotification", {
+  	Title = "Astro Star Outage";
+  	Text = "Astro Star Online is currently unavailable! Sorry for the inconvenience. Using Astro Star Offline.";
+	  Duration = 5;
+  })
+  src = game:HttpGet("https://raw.githubusercontent.com/antyhacker75-cmyk/MURDERER-MYSTERY-2-/refs/heads/main/MurdererMystery2v2.lua", false)
+end
+
+
+loadstring(src)()
