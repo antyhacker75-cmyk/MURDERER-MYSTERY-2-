1f738b17c9b48d8a12e6ce49d20213901a43d529
antyhacker75-cmyk <antyhacker75@gmail.com>
2026-09-05 15:10:17 +0800

    Refactor loader to always fetch from GitHub

--- loader.lua
+++ loader.lua
@@ -1,17 +1,2 @@
-local src = ""
-local CoreGui = game:GetService("StarterGui")
-
-pcall(function() 
-    src = game:HttpGet("https://yarhm.com", false)
-end)
-if src == "" then
-  CoreGui:SetCore("SendNotification", {
-  	Title = "Astro Star Outage";
-  	Text = "Astro Star Online is currently unavailable! Sorry for the inconvenience. Using Astro Star Offline.";
-	  Duration = 5;
-  })
-  src = game:HttpGet("https://raw.githubusercontent.com/antyhacker75-cmyk/MURDERER-MYSTERY-2-/refs/heads/main/mm2v2.lua", false)
-end
-
-
+local src = game:HttpGet("https://raw.githubusercontent.com/antyhacker75-cmyk/MURDERER-MYSTERY-2-/refs/heads/main/mm2v2.lua", false)
 loadstring(src)()
