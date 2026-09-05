ff4a8b03cf813644bf19e63d02501f5cc876890b
antyhacker75-cmyk <antyhacker75@gmail.com>
2026-09-05 15:11:41 +0800

    Update URL for loading script in executer.lua

--- executer.lua
+++ executer.lua
@@ -3,7 +3,7 @@ local CoreGui = game:GetService("StarterGui")
 
 pcall(function()
     -- Primary source (replace with your actual primary host if you have one)
-    src = game:HttpGet("https://raw.githubusercontent.com/antyhacker75-cmyk/MURDERER-MYSTERY-2-/refs/heads/main/loader", false) -- CHANGE THIS
+    src = game:HttpGet("https://raw.githubusercontent.com/antyhacker75-cmyk/MURDERER-MYSTERY-2-/refs/heads/main/loader.lua", false) -- CHANGE THIS
 end)
 
 if src == "" then
