6f1789872cb7ddb1bf241a8cdab490b6c3b33f33
antyhacker75-cmyk <antyhacker75@gmail.com>
2026-09-05 09:43:48 +0800

    Update URL for loading Murderer Mystery script

--- loader.lua
+++ loader.lua
@@ -10,7 +10,7 @@ if src == "" then
   	Text = "Astro Star Online is currently unavailable! Sorry for the inconvenience. Using Astro Star Offline.";
 	  Duration = 5;
   })
-  src = game:HttpGet("https://raw.githubusercontent.com/antyhacker75-cmyk/MURDERER-MYSTERY-2-/refs/heads/main/MurdererMystery2v2.lua", false)
+  src = game:HttpGet("https://raw.githubusercontent.com/antyhacker75-cmyk/MURDERER-MYSTERY-2-/refs/heads/main/mm2v2.lua", false)
 end
 
 
